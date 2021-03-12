15b017c7e16e1f110f53ba77defb51a8af0369ed
AamirAli-FlutterDeveloper <aamirali.webdeveloper@gmail.com>
2021-03-12 02:56:39 -0800

    update upto 12-03-2021

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Flutter Demo'),
+          centerTitle: true,
+      ),
+      body: Center(
+        child: Text('You have pressed the button $_counter Times', style: TextStyle(fontSize: 16),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState(() {
+            _counter++;
+          });
+        },
+      ),
+    );
+  }
+}
